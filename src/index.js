b306d0d393bb5d12abe3d6ea951c8da89843f989
Aliaksei Babko <alexeibobko@gmail.com>
2018-02-25 14:48:59 +0300

    solution to task 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let a, b, c, d;
+  let temp = [];
+  let solutions = [];
+  temp = equation.split(" ");
+  a = parseInt(temp[0]);
+  temp[3] == "-" ? b = (parseInt(temp[4]) * -1) : b = parseInt(temp[4]);
+  temp[7] == "-" ? c = (parseInt(temp[8]) * -1) : c = parseInt(temp[8]);
+  d = Math.sqrt((b * b) - (4 * a * c));
+  solutions[0] = Math.round((-b + d) / (2 * a));
+  solutions[1] = Math.round((-b - d) / (2 * a));
+  if (solutions[0] > solutions[1]) {
+    let z = solutions[0];
+    solutions[0] = solutions[1];
+    solutions[1] = z;
+  }
+  return solutions;
 }
